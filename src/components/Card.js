fda8fe15ea6cbfa83553f059f34dc4091952cc40
Alexander Kolchuk <forestrun342@gmail.com>
2023-10-16 17:12:06 +0300

    feat: 10th project

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Card (props) {
+
+  function handleClick () { props.onCardClick(props.card) }
+  function handleDelete () { props.onCardDelete(props.card) }
+
+  return (
+    <div className="cards__item">
+      <button type="button" className="cards__delete" onClick={ handleDelete } aria-label="Удалить" />
+      <img src={ props.link } className="cards__image" onClick={ handleClick } alt={ props.name } />
+      <div className="cards__info">
+        <h2 className="cards__description">{ props.name }</h2>
+        <div className="cards__like-area">
+          <button type="button" className="cards__like" aria-label="Like" />
+          <p className="cards__like-counter">{ props.likeCount > 0 ? props.likeCount : null }</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
